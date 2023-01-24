9f92c7109c2fc89d1e412322fad405d392c4c0b1
Pon Nirooba <90277335+nirooba98@users.noreply.github.com>
2023-01-24 14:39:27 -0500

    ContainsDuplicate solution

---
+++ solutions/ContainsDuplicate.py
@@ -0,0 +1,15 @@
+class Solution:
+    def containsDuplicate(self, nums: List[int]) -> bool:
+        my_hash = set()
+        for i in nums:
+            if i in my_hash:
+                return True
+            else:
+                my_hash.add(i)
+
+
+"""
+- Declare a new set 'my_hash'
+- Check if the integers in 'nums' array is already present in the set. Note that sets cannot have duplicate numbers.
+- When the condition is false, add the integer to the set. If the condition is true, return boolean value True.
+"""
